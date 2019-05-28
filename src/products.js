96660ba2180344728b91bed7f11f7e649e15af7f
Wilfried Kakou <mianquihin15@gmail.com>
2019-05-28 21:24:48 +0000

    Adding files.

---
+++ src/products.js
@@ -0,0 +1,74 @@
+const Products = [
+	{
+		"productSize": "",
+		"item": "DELUXE COOKED HAM",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "102",
+		"catId": "1",
+		"uom": "LB"
+	},
+	{
+		"productSize": "",
+		"item": "DELUXE LOW-SODIUM COOKED HAM ",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "159",
+		"catId": "1",
+		"uom": "LB"
+	},
+	{
+		"productSize": "",
+		"item": "DELUXE LOW-SODIUM WHOLE HAM",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "105",
+		"catId": "1",
+		"uom": "LB"
+	},
+	{
+		"productSize": "",
+		"item": "SMOKED VIRGINA HAM",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "156",
+		"catId": "1",
+		"uom": "LB"
+	},
+	{
+		"productSize": "",
+		"item": "HONEY MAPLE HAM",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "150",
+		"catId": "1",
+		"uom": "LB"
+	},
+	{
+		"productSize": "",
+		"item": "HONEY MAPLE HAM 1/2",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "151",
+		"catId": "1",
+		"uom": "LB"
+	},
+		{
+		"productSize": "",
+		"item": "BAVARIAN SMOKED HAM ",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "166",
+		"catId": "1",
+		"uom": "LB"
+	},
+	{
+		"productSize": "",
+		"item": "BLACK FOREST BEECHWOOD HAM L/S",
+		"plu_upc": "",
+		"price": " $5.15 ",
+		"productId": "11005",
+		"catId": "1",
+		"uom": "LB"
+	},
+]
